67ccf4d7c1bc295047743d364519ffb1c17d794e
Golujain21 <golujain2109@gmail.com>
2023-07-21 19:08:29 +0530

    initial changes

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
+import Resume from "../../assets/docs/resume.txt";
+import "./home.css";
+import { useTheme } from "../../context/ThemeContext";
+const Home = () => {
+  const [theme, setTheme] = useTheme();
+  //handle theme
+  const handleTheme = () => {
+    setTheme((prevState) => (prevState === "light" ? "dark" : "light"));
+  };
+  return (
+    <>
+      <div className="container-fluid home-container" id="home">
+        <div className="theme-btn" onClick={handleTheme}>
+          {theme === "light" ? (
+            <BsFillMoonStarsFill size={30} />
+          ) : (
+            <BsFillSunFill size={30} />
+          )}
+        </div>
+        <div className="container home-content">
+          <h2>Hi 👋 I'm a</h2>
+          <h1>
+            <Typewriter
+              options={{
+                strings: [
+                  "FullStack Developer!",
+                  "Mern Stack Developer!",
+                  "React native developer!",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h1>
+          <div className="home-buttons">
+            <button className="btn btn-hire">Hire Me</button>
+            <a className="btn btn-cv" href={Resume} download="your_name.pdf">
+              My Resume
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
